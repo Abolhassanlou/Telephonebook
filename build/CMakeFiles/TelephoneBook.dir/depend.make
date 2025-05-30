d1e4906b4e8fa89c1a10028b09e164ce47483d0f
Abolhassanlou <146092910+Abolhassanlou@users.noreply.github.com>
2025-05-30 23:21:28 +0200

    Delete build directory

--- build/CMakeFiles/TelephoneBook.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TelephoneBook.
-# This may be replaced when dependencies are built.
--- build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
- /home/hellen/Desktop/telbook-abolhassanlou3/build/_deps/catch2-src/src/catch2/internal/catch_result_type.cpp \
- /usr/include/stdc-predef.h \
- /home/hellen/Desktop/telbook-abolhassanlou3/build/_deps/catch2-src/src/catch2/../catch2/internal/catch_result_type.hpp
